c3bcde56afcdf74d0f0251feffc9de73e4e0c5e1
Lise Ekhorn Johansen <ljo177@uit.no>
2024-08-28 15:50:06 +0200

    union, difference, intersection done (not pretty but done)

--- in3120/postingsmerger.py
+++ in3120/postingsmerger.py
@@ -24,7 +24,28 @@ class PostingsMerger:
         The posting lists are assumed sorted in increasing order according
         to the document identifiers.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # Followed the pseudocode from the book (p. 48 figure 1.6)
+        try:
+            p1 = next(iter1)
+            p2 = next(iter2)
+            
+            while True:
+                if p1.document_id == p2.document_id:
+                    yield p1
+                    p1 = next(iter1)
+                    p2 = next(iter2)
+
+                elif p1.document_id < p2.document_id:
+                    p1 = next(iter1)
+
+                else:
+                    p2 = next(iter2)
+
+
+        except StopIteration:
+            return
+
+        # raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
 
     @staticmethod
     def union(iter1: Iterator[Posting], iter2: Iterator[Posting]) -> Iterator[Posting]:
@@ -35,7 +56,57 @@ class PostingsMerger:
         The posting lists are assumed sorted in increasing order according
         to the document identifiers.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+       
+        # Retrieve the first postings from the iterators, and check if the postings are empty
+        try:  
+            p1 = next(iter1)
+        except StopIteration:
+            p1 = None
+
+        try:
+            p2 = next(iter2)
+        except StopIteration:
+            p2 = None
+
+        # Loop through the postings as long as p1 nor p2 is None
+        while p1 is not None and p2 is not None:
+            # Doc_id is the same, yield one of them and advanve both iterators
+            if p1.document_id == p2.document_id:
+                yield p1
+                try:
+                    p1 = next(iter1)
+                except StopIteration:
+                    p1 = None # Has to set to empty, since stupid. 
+                   
+                try:
+                    p2 = next(iter2)
+                except StopIteration:
+                    p2 = None 
+                  
+            # Advance the first list to it matches the second list
+            elif p1.document_id < p2.document_id:
+                yield p1
+                try:
+                    p1 = next(iter1)
+                except StopIteration:
+                    p1 = None  
+            else:
+                yield p2
+                try:
+                    p2 = next(iter2)
+                except StopIteration:
+                    p2 = None
+                
+        # Yield the posting list that is not None, set to None in while loop. (Not a fan)
+        if p1 is not None:
+            yield p1
+            yield from iter1
+
+        if p2 is not None:
+            yield p2
+            yield from iter2
+      
+        # raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
 
     @staticmethod
     def difference(iter1: Iterator[Posting], iter2: Iterator[Posting]) -> Iterator[Posting]:
@@ -46,4 +117,45 @@ class PostingsMerger:
         The posting lists are assumed sorted in increasing order according
         to the document identifiers.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # Check if empty
+        try:
+            p1 = next(iter1)
+        except StopIteration:
+            p1 = None
+        try:
+            p2 = next(iter2)
+        except StopIteration:
+            p2 = None
+    
+        while p1 is not None and p2 is not None:
+            if p1.document_id == p2.document_id:
+
+                # Trying to iterate to the next posting
+                try:
+                    p1 = next(iter1)
+                except StopIteration:
+                    p1 = None
+                try:
+                    p2 = next(iter2)
+                except StopIteration:
+                    p2 = None
+
+            # Never intrested of yielding p2. So only yield p1 and move along. 
+            elif p1.document_id < p2.document_id:
+                yield p1
+                try:
+                    p1 = next(iter1)
+                except StopIteration:
+                    p1 = None
+            else:
+                try:
+                    p2 = next(iter2)
+                except StopIteration:
+                    p2 = None
+
+        # Yield the rest of the postings (for that time p2 is shorter than p1)
+        if p1 is not None:
+            yield p1
+            yield from iter1
+                        
+        # raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
